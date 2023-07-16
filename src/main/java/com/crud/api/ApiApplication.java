2f595073f0f090c8535b16b45427a8a9f7739155
GabrielDSant <gabrielbiel1597533@gmail.com>
2023-07-15 21:26:51 -0300

    Eu comitei antes sem buildar e não buildou...Agora builda

--- src/main/java/com/crud/api/ApiApplication.java
+++ src/main/java/com/crud/api/ApiApplication.java
@@ -3,7 +3,8 @@ package com.crud.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.crud.api.repository"})
+
 public class ApiApplication {
 
     public static void main(String[] args) {
