5dc24f7ba31568f5418ab6ac797afcaaea68eba3
GabrielDSant <gabrielbiel1597533@gmail.com>
2023-07-23 16:09:22 -0300

    O log4j2 ta funcionado mas não da forma que devia...

--- src/main/java/com/crud/api/ApiApplication.java
+++ src/main/java/com/crud/api/ApiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
     public static void main(String[] args) {
+        // Configuração do log4j2 usando o arquivo de configuração
+        System.setProperty("log4j.configurationFile", "log4j2.xml");
 
         SpringApplication.run(ApiApplication.class, args);
     }
--- src/main/java/com/crud/api/controller/ProductController.java
+++ src/main/java/com/crud/api/controller/ProductController.java
@@ -1,19 +1,18 @@
 // ProductController.java
 package com.crud.api.controller;
 
+import com.crud.api.model.Product;
+import com.crud.api.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.crud.api.repository.ProductRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.crud.api.model.Product;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @RestController
+
 @RequestMapping("/api/products")
 public class ProductController {
 
@@ -35,15 +34,13 @@ public class ProductController {
     }
 
     @PostMapping
-    public Product createProduct(@RequestBody Product product) {
-        return productRepository.save(product);
-    }
+    public Product createProduct(@RequestBody Product product) {return productRepository.save(product);}
 
     @PutMapping("/{id}")
     public Product updateProduct(@PathVariable Long id, @RequestBody Product updatedProduct) {
         return productRepository.findById(id)
                 .map(product -> {
-                    product.setName(updatedProduct.getName());
+                    product.setNome(updatedProduct.getNome());
                     product.setEmail(updatedProduct.getEmail());
                     return productRepository.save(product);
                 })
